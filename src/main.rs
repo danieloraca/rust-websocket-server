c5bcf33149005ba75389f2859584e2806a708f0f
Dan <daniel.oraca@gmail.com>
2024-08-27 20:07:32 +0100

    ulid client name

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use futures_util::{SinkExt, StreamExt};
+use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::{mpsc, RwLock};
+use uuid::Uuid;
 use warp::ws::{Message, WebSocket};
 use warp::Filter;
 
-type Clients = Arc<RwLock<Vec<mpsc::UnboundedSender<Message>>>>;
+#[derive(Clone)]
+struct Client {
+    name: String,
+    sender: mpsc::UnboundedSender<Message>,
+}
+
+type Clients = Arc<RwLock<HashMap<Uuid, Client>>>;
 
 #[tokio::main]
 async fn main() {
@@ -34,17 +42,31 @@ async fn handle_connection(ws: WebSocket, clients: Clients) {
     let (mut tx, mut rx) = ws.split();
     let (client_tx, mut client_rx) = mpsc::unbounded_channel();
 
-    // Add this client to the list of connected clients
-    clients.write().await.push(client_tx.clone());
+    // Generate a unique name for the client
+    let client_name = format!("Client-{}", Uuid::new_v4());
+
+    // Create a client instance and add it to the list of connected clients
+    let client = Client {
+        name: client_name.clone(),
+        sender: client_tx.clone(),
+    };
+    clients.write().await.insert(Uuid::new_v4(), client);
 
     // Task to receive messages from this client and broadcast them to others
     let clients_clone = clients.clone();
     tokio::spawn(async move {
         while let Some(result) = rx.next().await {
             if let Ok(msg) = result {
+                let msg = Message::text(format!("{}: {}", client_name, msg.to_str().unwrap_or("")));
                 broadcast_message(msg, &clients_clone).await;
             }
         }
+
+        // Remove the client when they disconnect
+        clients_clone
+            .write()
+            .await
+            .retain(|_, c| c.name != client_name);
     });
 
     // Task to send messages from the broadcast channel to this client
@@ -61,7 +83,7 @@ async fn handle_connection(ws: WebSocket, clients: Clients) {
 async fn broadcast_message(msg: Message, clients: &Clients) {
     let clients = clients.read().await;
 
-    for client in clients.iter() {
-        let _ = client.send(msg.clone());
+    for client in clients.values() {
+        let _ = client.sender.send(msg.clone());
     }
 }
